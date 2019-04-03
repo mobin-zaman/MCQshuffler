83de3e3d598eb042b4bb0b8c99236eec3db67538
mobin-zaman <mobin_zaman@hotmail.com>
2019-04-03 15:05:28 +0600

    last insert id added in DB

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import classes.Student;
 import java.sql.SQLException;
 import java.util.*;
 
 
 import classes.*;
+import database.DB;
+
 import org.apache.commons.dbutils.*;
 import org.apache.commons.dbutils.handlers.*;
 
@@ -13,46 +14,44 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        // final String url = "jdbc:mysql://localhost:3306/mcq?useSSL=false";
-        // final String driver = "com.mysql.jdbc.Driver";
-        // final String usr = "phpmyadmin";
-        // final String pwd = "amarsql";
-        // Connection conn=null;
-
-        // QueryRunner run = new QueryRunner();
-
-        // System.out.println("1");
-        // DbUtils.loadDriver(driver);
-        // try{
-        //     System.out.println("this");
-        // conn = DriverManager.getConnection(url, usr, pwd);
-        // System.out.println(3);
-        // }
-        // catch(Exception e){
-        //     System.out.println("ex: "+e);
-        // }
-        // System.out.println("2");
-        // -----------------------------------------------------------------------------------
-
         DB db = new DB();
 
-        ResultSetHandler <List<Question>> resultSetHandler=new BeanListHandler<Question>(Question.class);
+        // ResultSetHandler <List<Question>> resultSetHandler=new BeanListHandler<Question>(Question.class);
 
-        List<Question> questionList = db.run.query(db.getConn(),"SELECT * FROM question where courseId=?",resultSetHandler,"1");
-        Collections.shuffle(questionList);
+        // List<Question> questionList = db.run.query(db.getConn(),"SELECT * FROM question where courseId=?",resultSetHandler,"3");
+        // Collections.shuffle(questionList);
 
-        for(Question q:questionList){
-            System.out.println("id: "+q.getId());
-            System.out.println("description: "+q.getDescription());
-            System.out.println("choice 1"+q.getChoiceOne());
-            System.out.println("choice 2"+q.getChoiceTwo());
-            System.out.println("choice 3"+q.getChoiceThree());
-            System.out.println("choice 4"+q.getChoiceFour());
-            System.out.println();
-            System.out.println("correct answer"+q.getCorrectChoice());
-        }
+        // System.out.println("length: "+questionList.size());
+        // for(Question q:questionList){
+        //     System.out.println("id: "+q.getId());
+        //     System.out.println("description: "+q.getDescription());
+        //     System.out.println("choice 1"+q.getChoiceOne());
+        //     System.out.println("choice 2"+q.getChoiceTwo());
+        //     System.out.println("choice 3"+q.getChoiceThree());
+        //     System.out.println("choice 4"+q.getChoiceFour());
+        //     System.out.println();
+        //     System.out.println("correct answer"+q.getCorrectChoice());
+        // }
+
+        // String sql="Insert into exam (courseId,Description) Values(?,?)";
+        
+        // int insertedRecords=db.run.update(db.getConn(),sql,"1","first exam ever");
+        // System.out.println(insertedRecords);
+        
+        createExam("test exam",3,1);
 
 
-        }
+    }
        
+        static void createExam(String description, int numberOfQuestions, int courseId){
+        DB db = new DB();
+        String sql="Insert into exam(courseId,Description) Values(?,?)";
+        try{
+        int insertedId=db.run.update(db.getConn(),sql,courseId,description);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("insertid: "+db.lastInsertId());
+        }
+    
 }
--- database/DB.java
+++ database/DB.java
@@ -1,7 +1,9 @@
 package database;
+import java.math.BigInteger;
 import java.sql.*;
 import classes.Databaseproperty;
 import org.apache.commons.dbutils.*;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 public class DB{ 
     private Connection connection;
@@ -16,10 +18,20 @@ public class DB{
         
         try{ connection=DriverManager.getConnection(dbproperty.url(), dbproperty.username(),dbproperty.password());
         } catch(Exception e){ System.out.println("exception in DB constructor: "+e);}
-        System.out.println("db initiating done");
+        System.out.println("db initiation done");
 
     }
     public Connection getConn(){
         return connection;
+    }
+    public int lastInsertId(){
+        int insertId=0;
+        try{
+        insertId=((BigInteger)this.run.query(this.connection,"SELECT LAST_INSERT_ID()",new ScalarHandler(1))).intValue();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        return insertId;
+        
     }
 }
