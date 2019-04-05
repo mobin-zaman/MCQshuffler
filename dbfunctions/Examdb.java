753180e8954fbb940de27bb15699c8db396b7ecb
mobin-zaman <mobin_zaman@hotmail.com>
2019-04-05 10:49:53 +0600

    insertQuestion() added

--- dbfunctions/Examdb.java
+++ dbfunctions/Examdb.java
@@ -1,35 +1,40 @@
 package dbfunctions;
+
 import database.DB;
 import classes.*;
 import java.util.List;
 
-public class Examdb{
-public static void createExam(String description, int numberOfQuestions, int courseId){
-    DB db = new DB();
-    //fist insert the exam description, then retrieve id
-    String sql="Insert into exam(courseId,Description) Values(?,?)";
-    try{
-        db.run.update(db.getConn(),sql,courseId,description);
-    } catch (Exception e){
-        System.out.println(e);
-    }
-    //retrieving done
-    int examId=db.lastInsertId();
-    System.out.println("insertid: "+examId);
-    
-    //now get Question of the corresponding course for the exam
-    //we get it by the courseId parameter
-    List <Question> questionList =Questiondb.getRandomQuestionList(courseId);
+public class Examdb {
+    public static void createExam(String description, int numberOfQuestions, int courseId) {
+        DB db = new DB();
+        // fist insert the exam description, then retrieve id
+        String sql = "Insert into exam(courseId,Description) Values(?,?)";
+        try {
+            db.run.update(db.getConn(), sql, courseId, description);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // retrieving done
+        int examId = db.lastInsertId();
+        System.out.println("insertid: " + examId);
 
-    //now insert into exam_relations table, given by numberOfQuestions
+        // now get Question of the corresponding course for the exam
+        // we get it by the courseId parameter
+        List<Question> questionList = Questiondb.getRandomQuestionList(courseId);
 
-    //sql for the insertion
+        // now insert into exam_relations table, given by numberOfQuestions
 
-    sql="Insert into exam_question(examId,questionId) Values(?,?)";
-        for(int i=0;i<numberOfQuestions;i++){
-        int x=questionList.get(i).getId();
-        try { db.run.update(db.getConn(), sql, examId,x);
-          } catch(Exception e) {System.out.println("createExam: "+e);}
+        // sql for the insertion
+        sql = "Insert into exam_question(examId,questionId) Values(?,?)";
+        for (int i = 0; i < numberOfQuestions; i++) {
+            int x = questionList.get(i).getId();
+            try {
+                db.run.update(db.getConn(), sql, examId, x);
+            } catch (Exception e) {
+                System.out.println("createExam: " + e);
+            }
         }
     }
+
+    //
 }
--- dbfunctions/Questiondb.java
+++ dbfunctions/Questiondb.java
@@ -1,32 +1,47 @@
 package dbfunctions;
+
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.*;
 import database.DB;
 import classes.Question;
 import java.util.*;
-public class Questiondb{
-    //self explanatory name
-    public static List<Question> getRandomQuestionList(int courseId){
-        
-        List<Question>questionList=getAllQuestionList(courseId);
+
+public class Questiondb {
+    // self explanatory name
+    public static List<Question> getRandomQuestionList(int courseId) {
+
+        List<Question> questionList = getAllQuestionList(courseId);
         Collections.shuffle(questionList);
         return questionList;
     }
 
-    //the function needed for showing the question bank in courses
-    //also needed for creating exam
-    public static List<Question> getAllQuestionList(int courseId){
-        DB db=new DB();
-        //needed for passing to db.run.query()
-        ResultSetHandler<List<Question>> resultSetHandler=new BeanListHandler<Question>(Question.class);
-        //needed for getting the results from table
-        List<Question> questionList=null;
-        //retrieve from database
-        try{
-            questionList = db.run.query(db.getConn(),"SELECT id FROM question where courseId=?",resultSetHandler,courseId);
-        } catch(Exception e){
-            System.out.println("getQuestionList(): "+e);
+    // the function needed for showing the question bank in courses
+    // also needed for creating exam
+    public static List<Question> getAllQuestionList(int courseId) {
+        DB db = new DB();
+        // needed for passing to db.run.query()
+        ResultSetHandler<List<Question>> resultSetHandler = new BeanListHandler<Question>(Question.class);
+        // needed for getting the results from table
+        List<Question> questionList = null;
+        // retrieve from database
+        try {
+            questionList = db.run.query(db.getConn(), "SELECT id FROM question where courseId=?", resultSetHandler,
+                    courseId);
+        } catch (Exception e) {
+            System.out.println("getQuestionList(): " + e);
         }
         return questionList;
+    }
+
+    // for inserting a single question
+    public static void insertQuestion(int courseid, Question q) {
+        DB db = new DB();
+        String sql = "Insert into question (courseId,description,choiceOne,choiceTwo,choiceThree,choiceFour,correctChoice) Value(?,?,?,?,?,?,?)";
+        try {
+            db.run.update(db.getConn(), sql, q.getCourseId(), q.getDescription(), q.getChoiceOne(), q.getChoiceTwo(),
+                    q.getChoiceThree(), q.getChoiceFour(), q.getCorrectChoice());
+        } catch (Exception e) {
+            System.out.println("insertQuestion(): " + e);
+        }
     }
 }
