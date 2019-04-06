a9cfd58e7e0968bd31530f4bf8271bdc1c825053
Sadat Jubayer <sadatjubayer@gmail.com>
2019-04-06 11:38:10 -0700

    gui version 1

---
+++ GUI/AddCourse.java
@@ -0,0 +1,54 @@
+import java.lang.*;
+import javax.swing.*;
+
+public class AddCourse extends JFrame {
+    private JLabel courseName, courseCode, credit, title;
+    private JTextField courseNameField, courseCodeField, creditField;
+    private JButton addCourse;
+    private JPanel panel;
+
+    public AddCourse() {
+
+        super("Add course");
+        this.setSize(800, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setLayout(null);
+
+        title = new JLabel("Add Course");
+        title.setBounds(250, 50, 200, 30);
+        panel.add(title);
+
+        courseName = new JLabel("Course Name: ");
+        courseName.setBounds(150, 100, 100, 30);
+        panel.add(courseName);
+
+        courseNameField = new JTextField();
+        courseNameField.setBounds(250, 100, 100, 30);
+        panel.add(courseNameField);
+
+        courseName = new JLabel("Course Code: ");
+        courseName.setBounds(150, 150, 100, 30);
+        panel.add(courseName);
+
+        courseNameField = new JTextField();
+        courseNameField.setBounds(250, 150, 100, 30);
+        panel.add(courseNameField);
+
+        courseName = new JLabel("Credit(s): ");
+        courseName.setBounds(150, 200, 100, 30);
+        panel.add(courseName);
+
+        courseNameField = new JTextField();
+        courseNameField.setBounds(250, 200, 100, 30);
+        panel.add(courseNameField);
+
+        addCourse = new JButton("Add Course");
+        addCourse.setBounds(200, 250, 150, 30);
+        panel.add(addCourse);
+
+        this.add(panel);
+
+    }
+}
---
+++ GUI/Exam.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+import javax.swing.*;
+
+public class Exam extends JFrame {
+    private JLabel questionList, sampleQues;
+    private JButton examButton, studentButton;
+
+    private JPanel panel;
+
+    public Exam() {
+
+        super("Exam");
+        this.setSize(800, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setLayout(null);
+
+        questionList = new JLabel("Avaibale Exams");
+        questionList.setBounds(380, 50, 150, 30);
+        panel.add(questionList);
+
+        examButton = new JButton("Marks");
+        examButton.setBounds(100, 100, 150, 30);
+        panel.add(examButton);
+
+        studentButton = new JButton("Create Exam");
+        studentButton.setBounds(100, 300, 150, 30);
+        panel.add(studentButton);
+
+        // List of questions will be here
+
+        sampleQues = new JLabel("Exam. hahaahahahahahahahahahahaahaha");
+        sampleQues.setBounds(380, 100, 500, 20);
+        panel.add(sampleQues);
+
+        this.add(panel);
+
+    }
+}
