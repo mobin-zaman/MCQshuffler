cf7bf25e205e873083a8ec2c080bf078154c322c
mobin-zaman <mobin_zaman@hotmail.com>
2019-04-11 11:38:19 +0600

    .

--- Main.java
+++ Main.java
@@ -14,14 +14,14 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        System.out.println(Studentdb.isRejected("1", "2"));
+        // System.out.println(Studentdb.isRejected("1", "2"));
 
-        // List<Course> course = Coursedb.getCourseList("1");
-        // course.forEach((c) -> {
-        // System.out.println(c.getId());
-        // System.out.println(c.getName());
-        // System.out.println(c.getTeacherId());
-        // });
+        List<Course> course = Coursedb.getCourseList("1");
+        course.forEach((c) -> {
+        System.out.println(c.getId());
+        System.out.println(c.getName());
+        System.out.println(c.getTeacherId());
+        });
 
         // ResultSetHandler <List<Question>> resultSetHandler=new
         // BeanListHandler<Question>(Question.class);
